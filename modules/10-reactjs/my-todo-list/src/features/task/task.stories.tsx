e6dad73d9c261fd2ff762d4938267315ed08abb4
meji <soy@meji.es>
2020-03-04 20:50:55 +0100

    pasword con opcion select

--- modules/10-reactjs/my-todo-list/src/features/task/task.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Task } from './task'
-
-export default {
-  title: 'Task',
-  component: Task
-}
-
-export const taskIncompleted = () => <Task text="Tarea" icon="unchecked"/>
